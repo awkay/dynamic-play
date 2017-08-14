3c6e1c4e95e10e407eb1ac50e00c8696bfcdc381
Tony Kay <tony.kay@gmail.com>
2017-08-14 12:58:08 -0700

    added helper scripts

---
+++ build.sh
@@ -0,0 +1,1 @@
+rm -rf resources/public/js/demos; lein run -m clojure.main build.clj
--- project.clj
+++ project.clj
@@ -7,20 +7,4 @@
                  [org.clojure/clojurescript "1.9.894" ]
                  [fulcrologic/fulcro "1.0.0-beta8-SNAPSHOT"]]
 
-  :source-paths ["src"]
-  :clean-targets ^{:protect false} ["resources/private/js" "resources/public/js" "target"]
-
-  :plugins [[lein-cljsbuild "1.1.7"]]
-
-  :cljsbuild {:builds
-              [ {:id           "demos"
-                :source-paths ["src"]
-                :compiler     {:output-dir    "resources/public/js/demos"
-                               :asset-path    "js/demos"
-                               :modules       {:entry-point {:output-to "resources/public/js/demos/demos.js"
-                                                             :entries   #{recipes.dynamic-ui-routing}}
-                                               :main        {:output-to "resources/public/js/demos/main-ui.js"
-                                                             :entries   #{recipes.dynamic-ui-main}}}
-                               :verbose       true
-                               :optimizations :advanced}}
-               ]})
+  :source-paths ["src"])
